60fada80b6e782994e5d4a63f769f0f0f4e366b6
wulinan <wulinan_wln@126.com>
2016-07-18 12:05:53 +0800

    new file

--- problems/src/lca235/Solution.java
+++ problems/src/lca235/Solution.java
@@ -1,5 +1,8 @@
 package lca235;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import sameTree100.TreeNode;
 
 public class Solution {
@@ -17,4 +20,36 @@ public class Solution {
 		if(root==p) return true;
 		return dfs(root.left,p) || dfs(root.right,p);
 	}
+	
+	public TreeNode lowestCommonAncestor1(TreeNode root, TreeNode p, TreeNode q) {
+        List<TreeNode> list1 = new ArrayList<TreeNode>();
+        List<TreeNode> list2 = new ArrayList<TreeNode>();
+        getNodePath(list1,root,p);
+        getNodePath(list2,root,q);
+        return getCommonParent(list1,list2);
+    }
+    
+    public boolean getNodePath(List<TreeNode> list,TreeNode root,TreeNode node){
+    	if(root==null) return false;
+        list.add(root);
+        if(root==node) return true;
+        boolean found = getNodePath(list,root.left,node) || getNodePath(list,root.right,node);
+        if(!found) list.remove(root);
+        return found;
+        
+    }
+    
+    public TreeNode getCommonParent(List<TreeNode> list1,List<TreeNode> list2){
+        int length1 = list1.size();
+        int length2 = list2.size();
+        TreeNode common = null;
+        int i = 0;
+        while(i<length1 && i<length2){
+            if(list1.get(i)==list2.get(i)){
+                common = list1.get(i);
+                i++;
+            }else break;    
+        }
+        return common;
+    }
 }
